795226cddb7def32aed20b3b1847d59a03e91aef
letifelse <let.if.else@gmail.com>
2020-10-15 16:33:38 +0400

    training.js

--- training.js
+++ training.js
@@ -1,3 +1,26 @@
-let f = 1;
-let d = 2;
-console.log (f + d);
+console.log (-0.304);
+
+let fed = "fed";
+console.log (fed);
+
+console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`);
+
+console.log ('7' - (-8 - -2));
+
+import { reverse } from 'hexlet-basics/string';
+const result = revers('Hello!');
+console.log(result);
+
+const dist = calculateDistance ('Qarth', 'Vaes Dothrak');
+console.log(dist);
+
+console.log(Math.abs(soldiersCount));
+
+console.log(Math.ceil(number));
+
+console.log(round(number, 2));
+
+console.log(Math.min(3, 10, 22, -3, 0));
+
+const distance = calculateDistance('Winterfell', 'The Twins') + calculateDistance('The Twins', 'The Eyrie');
+console.log(distance);
